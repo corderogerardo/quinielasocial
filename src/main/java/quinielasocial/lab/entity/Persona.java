93f7b88edf6cc580cc7180017b35db0e451239d2
Gerardo Cordero <cordero.gerard@gmail.com>
2016-03-11 19:28:03 -0430

    Persona entity started

---
+++ src/main/java/quinielasocial/lab/entity/Persona.java
@@ -0,0 +1,13 @@
+package quinielasocial.lab.entity;
+
+public class Persona {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String foto;
+	
+	
+
+}
